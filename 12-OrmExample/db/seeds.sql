b555fb4099a61b07f180aca5bf1cc4b72ba52b1a
Wes Hatley <weshatley@gmail.com>
2019-03-16 10:15:21 -0400

    Initial commit without Solutions included

---
+++ 12-OrmExample/db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO buyers (buyer_name) VALUES ('Kevon');
+INSERT INTO buyers (buyer_name) VALUES ('Jane');
+INSERT INTO buyers (buyer_name) VALUES ('Tim');
+
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('wolf', 'Cassie', 195, 1);
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('bear', 'Rachel', 745, 1);
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('hawk', 'Tobias', 850, 2);
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('gorilla', 'Marco', 400, 2);
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('tiger', 'Jake', 300, 2);
+INSERT INTO pets (animal_breed, animal_name, price, buyer_id) VALUES ('boxer', 'Le Frou Frou', 2200, 3);
---
+++ 17-CatsApp/Unsolved/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobbi');
+INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO cats (name, sleepy) VALUES ('Archer', true);
+INSERT INTO cats (name, sleepy) VALUES ('Joey', true);
+INSERT INTO cats (name) VALUES ('Owens');
